d3b9ccc110a76827feeea15e9c469e426f2aa049
Kipkorir2017 <pkobenj41@gmail.com>
2021-10-08 21:12:31 +0300

    add changes to the view file

--- gallery/urls.py
+++ gallery/urls.py
@@ -3,4 +3,7 @@ from .import views
 
 urlpatterns = [
     url('^$',views.displayhome,name = 'welcome'),
+    url(r'^search/', views.search_category, name='search_category'),
+    url('^image/(?P<image_id>\d+)/$',views.image_properties, name='image'),
+    url(r'^location/(?P<location_name>\w+)',views.image_location,name = 'location'),
 ]
