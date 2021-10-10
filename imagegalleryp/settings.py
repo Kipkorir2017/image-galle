d56aa4e2f21bbb880301536cd188ed37f4f894bf
Kipkorir2017 <pkobenj41@gmail.com>
2021-10-10 15:10:15 +0300

    import claudinary in the settings

--- imagegalleryp/settings.py
+++ imagegalleryp/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -40,6 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary',
+
 ]
 
 MIDDLEWARE = [
@@ -134,3 +139,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+cloudinary.config( 
+  cloud_name = "bentec", 
+  api_key = "845195192227671", 
+  api_secret = "FkrrelGjjHOBQhkamdIUVzyggf8" 
+)
